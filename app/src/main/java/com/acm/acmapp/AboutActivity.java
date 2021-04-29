65479ab98f6fe8bb66f98548558f3c432d5056cf
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-04-29 17:25:37 -0400

    Recuperamos los datos enviados en otra Activity

--- app/src/main/java/com/acm/acmapp/AboutActivity.java
+++ app/src/main/java/com/acm/acmapp/AboutActivity.java
@@ -6,11 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+    EditText txtName;
+    TextView txtView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        txtName = findViewById(R.id.txtName);
+        txtView = findViewById(R.id.txtView);
+
+        String datos = "Email: "+getIntent().getStringExtra("email")
+                +"\nDireccion:"+getIntent().getStringExtra("address")+"\nDepartamentos: "+getIntent().getStringExtra("deptos")
+                +"\nHorario: "+getIntent().getStringExtra("timetable");
+        txtName.setText(getIntent().getStringExtra("name"));
+        txtView.setText(datos);
     }
 }
